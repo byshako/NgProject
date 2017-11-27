7c14d84e2d4380a234f06d074fffa6b31a7712cb
byshako <byshako@gmail.com>
2017-11-27 15:48:13 +0400

    New Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
