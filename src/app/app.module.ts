7a5d186e11f4812097ea71a7c19eff9873075cf9
byshako <byshako@gmail.com>
2017-11-27 16:26:08 +0400

    Shopping Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ShoppingComponent } from './components/shopping/shopping.component';
-import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './components/shopping/shopping-list-edit/shopping-list-edit.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
